b5558de89b8617d2553cc80b2a2aa3f9a6b6e719
karlosvas <carlosvassan@gmail.com>
2025-05-13 22:48:34 +0200

    DOMjudge-2024 p90Factorivisores

---
+++ DOMjudge-2024/p90FactoDivisores/FactoDivisores.java
@@ -0,0 +1,74 @@
+package p90FactoDivisores;
+
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class FactoDivisores {
+    static Scanner in;
+
+    public static boolean solve() {
+        if (!in.hasNextLong()) return false;
+
+        long f = in.nextLong();
+        long d = in.nextLong();
+
+        if (d == 0) {
+            System.out.printf("%d no divide a %d!%n", d, f);
+            return true;
+        }
+
+        if (isDivisibleByFactorial(f, d)) {
+            System.out.printf("%d divide a %d!%n", d, f);
+        } else {
+            System.out.printf("%d no divide a %d!%n", d, f);
+        }
+
+        return true;
+    }
+
+    public static boolean isDivisibleByFactorial(long f, long d) {
+        long remaining = d;
+
+        for (long i = 2; i * i <= remaining; i++) {
+            int count = 0;
+            while (d % i == 0) {
+                d /= i;
+                count++;
+            }
+
+            // Contar cuántas veces aparece i en f!
+            long factorCount = 0;
+            for (long j = i; j <= f; j *= i) {
+                factorCount += f / j;
+            }
+
+            if (factorCount < count)
+                return false;
+        }
+
+        // Si queda un primo mayor que sqrt(d)
+        if (d > 1) {
+            long factorCount = 0;
+            for (long j = d; j <= f; j *= d) {
+                factorCount += f / j;
+            }
+
+            if (factorCount < 1)
+                return false;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+        boolean isDebug = false;
+        if (isDebug)
+            in = new Scanner(new java.io.File("DOMjudge-2024/p90FactoDivisores/input.txt"));
+        else
+            in = new Scanner(System.in);
+
+        while (solve());
+
+        in.close();
+    }
+}
